4279e033a04a85a5f1ef6fe7be765196e569dbad
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-25 07:38:31 -0700

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:
+    process.env.REACT_APP_API_URL
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(
+      "API Error:",
+      error?.response?.data || error.message
+    );
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
